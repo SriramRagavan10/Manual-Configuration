8d229518919fd86477b6c4633a1b78b8847d017c
SriramRagavan10 <ramsri078@gmail.com>
2024-04-25 21:08:24 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { KaviyamComponent } from './kaviyam.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KaviyamComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/kaviyam.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-sample',
+    templateUrl:'./kaviyam.component.html',
+    styleUrls:['./kaviyam.component.css']
+})
+
+export class KaviyamComponent {
+    title ="Puthu Ulagam";
+
+}
